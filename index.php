307979e89671038a10a8e1f225e857b1f9c34b47
Alireza Savand <alireza.savand@gmail.com>
2018-06-07 16:43:53 +0300

    Make index.php much nicer with smarty

--- index.php
+++ index.php
@@ -1,49 +1,20 @@
-
-	<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Money lover</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta charset="utf-8">
- <?php
-include("ass.php");
-?>
-
-
- <body class='bgimg'>
-<script>
-$(document).ready(function(){
-    
-        $("#p").hide();
-});
-</script>
-
- <style>
-.bgimg {
-    background-position: cover;
-    background-size: 100% 100%;
-    background-image: url("picture/cash_and_gold-300x239-1.jpg");
-    min-height: 100%;
-	   box-shadow: inset 0 0 0 10000px rgba(0.7, 0.9, 0.9, 0.19);
-}
-</style>
- 
 <?php
- include 'configg.php';
- $sql = "SELECT * FROM user WHERE username IS NOT NULL";  
-                    $query =  mysqli_query($conn, $sql);
-                    $rows = mysqli_num_rows($query);
-                    if($rows>0)
-                    {
-		 
-		  include("passwordindex.php");
-					}
-					
-					else{
-	
-	include("passwordindexnot.php");
-					}
- ?>
-<!-- the end -->	
- </body>
-</html>
+include("libs/smarty/Smarty.class.php");
+include 'configg.php';
+
+$sql = "SELECT * FROM user WHERE username IS NOT NULL";
+$query = mysqli_query($conn, $sql);
+$rows = mysqli_num_rows($query);
+$password_template = null;
+
+if ($rows > 0) {
+    $password_template = "passwordindex.php";
+} else {
+    $password_template = "passwordindexnot.php";
+}
+
+$smarty = new Smarty;
+$smarty->assign('title', 'Money lover');
+$smarty->assign('password_template', $password_template);
+
+$smarty->display('index.tpl');
