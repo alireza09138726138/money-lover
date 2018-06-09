a23bd38c89214d6504c0a94de8c7a1dab8498756
Alireza Savand <alireza.savand@gmail.com>
2018-06-09 18:24:27 +0300

    Add sidebarmenu to the index.tpl

--- asset.php
+++ asset.php
@@ -2,9 +2,8 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script language="javascript" type="text/javascript" src="ajax.js"></script>
-<script type="text/javascript"
-        src="http://www.phpzag.com/demo/delete-records-with-bootstrap-confirm-modal-using-php-mysql/script/bootbox.min.js"></script>
+<script language="javascript" type="text/javascript" src="assets/js/app.js"></script>
+<script type="text/javascript" src="http://www.phpzag.com/demo/delete-records-with-bootstrap-confirm-modal-using-php-mysql/script/bootbox.min.js"></script>
 <link rel="shortcut icon" href="picture/pexels-photo-221174.png.jpg"/>
 
 <!-- font-awesome for sidebar -->
