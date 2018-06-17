17fe241ea96719684cda534c3bda85eec774a246
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-06-18 02:01:01 +0430

    update home.php

--- home.php
+++ home.php
@@ -1,60 +1,11 @@
-	<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Money lover</title>
-  <meta charset="utf-8">
- <?php
-  
-include("navcost.php");
+	<?php
+include("libs/smarty/Smarty.class.php");
+include 'configg.php';
+$date=date("d-m-Y ");;
+
+$smarty = new Smarty;
+$smarty->assign('title', 'Money lover');
+
+$smarty->assign('date', $date);
+$smarty->display('passwordindexnot.tpl');
 ?>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
- 
- 
- <?php
-
-  include("js.php");
-
-?>
- 
-</head>
-<!-- body -->
- <body>
- 
-
-<!-- Header with image -->
-<header class="bgimg w3-display-container w3-grayscale-min" id="home">
-
-
-<span class="w3-button w3-xxlarge  w3-right" style='color:white' onclick="w3_open()"><i class="fa fa-bars"></i></span> 
-
-
-  
-  <div class="w3-display-middle w3-center c" >
-    <span class="w3-text-white w3-jumbo w3-animate-top" style="font-size:90px;font-family:weight"><b>MONEY<br>LOVER</b></span>
-  </div>
-  
-  <div class="w3-display-bottomright w3-center w3-padding-large">
-    <span class="w3-text-white"><h3 style='color:white' id='date'>28-05-2018 </h3></span>
-  </div>
-</header>
-
-
-
-
-<style>
-
-body, html {
-    height: 100%;
-    font-family: "Inconsolata", sans-serif;
-	background-color:black;
-}
-.bgimg {
-    background-position: cover;
-    background-size: 100% 100%;
-    background-image: url("picture/cash_and_gold-300x239-1.jpg");
-    min-height: 100%;
-	  box-shadow: inset 0 0 0 10000px rgba(0.7, 0.9, 0.9, 0.19);
-}
-
-</style>
