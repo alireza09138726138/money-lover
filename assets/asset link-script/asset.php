0da880df558cb06abbd4030fd152a86eaed2c469
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-06-18 00:32:24 +0430

    asset

---
+++ assets/asset link-script/asset.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="assets/style/style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="shortcut icon" href="picture/pexels-photo-221174.png.jpg"/>
+<!-- font-awesome for sidebar -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script language="javascript" type="text/javascript" src="ajax.js"></script>
+<script language="javascript" type="text/javascript" src="assets/js/app.js"></script>
+<script type="text/javascript" src="http://www.phpzag.com/demo/delete-records-with-bootstrap-confirm-modal-using-php-mysql/script/bootbox.min.js"></script>
