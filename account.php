a2e3508f86ae9d57023d99ade7124fe2669cfba5
Alireza Savand <alireza.savand@gmail.com>
2018-06-21 20:31:11 +0300

    Fix duplicate row

--- account.php
+++ account.php
@@ -1,21 +1,19 @@
-
 <?php
 include("libs/smarty/Smarty.class.php");
 include 'configg.php';
 
 $date = date("d-m-Y ");
 
-$sql = "SELECT * FROM produc WHERE year IS NULL";
-$query = mysqli_query($conn, $sql);
-
-while ($ro[] = mysqli_fetch_assoc($query)){};
-
-
-
+// By default, produ.year is None and not NULL.
+// That's why the following query will not return anything since
+// all the rows in database will be created with year=None by default.
+// Null is different than None!
+$sql = "SELECT * FROM `produ` WHERE `year` IS NULL";
+$result = mysqli_query($conn, $sql);
+$rows = mysqli_fetch_all ($result, MYSQLI_ASSOC);
 
 
 $smarty = new smarty();
-$smarty->assign('ro', $ro);
+$smarty->assign('rows', $rows);
 $smarty->display("account.tpl");
-?>
 
