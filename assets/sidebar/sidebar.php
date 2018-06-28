c8dc26e69dedb75d8ea2cff1e4629532558b9650
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-06-28 20:42:09 +0430

    Update sidebar.php

--- assets/sidebar/sidebar.php
+++ assets/sidebar/sidebar.php
@@ -1,16 +1,17 @@
 
+
 <!-- Sidebar -->
-<nav class="w3-sidebar w3-black w3-animate-top w3-xxlarge" style="display:none;padding-top:150px" id="mySidebar">
-  <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-black w3-xxlarge w3-padding w3-display-topright" style="padding:6px 24px">
+<nav class="w3-sidebar w3-black w3-animate-top w3-xxlarge nav"  id="mySidebar">
+  <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-black w3-xxlarge w3-padding w3-display-topleft" >
     <i class="fa fa-remove"></i>
   </a>
     <div class="w3-bar-block w3-center">
-    <a href="home.php" class="w3-bar-item w3-button w3-text-grey w3-hover-black"><i class="fa fa-home" style="font-size:18px;color:red">&nbsp;&nbsp;</i><h3>Home</h3></a>
+    <a href="home.php" class="w3-bar-item w3-button w3-text-grey w3-hover-black"><i class="fa fa-home navv">&nbsp;&nbsp;</i><h3>Home</h3></a>
 	<a href="explain.php" class="w3-bar-item w3-button w3-text-grey w3-hover-black"><h3>Explain</h3></a>
-    <a href="Username.php" class="w3-bar-item w3-button w3-text-grey w3-hover-black"><i class="fa fa-key" style="font-size:18px;color:red">&nbsp;&nbsp;</i><h3>Creat password</h3></a>
+    <a href="Username.php" class="w3-bar-item w3-button w3-text-grey w3-hover-black"><i class="fa fa-key navv" >&nbsp;&nbsp;</i><h3>Creat password</h3></a>
 	<a href="account.php" class="w3-bar-item w3-button w3-text-grey w3-hover-black"><h3>Creat new account</h3></a>
     <a href="buy.php" class="w3-bar-item w3-button w3-text-grey w3-hover-black"><h3>Cost management(buy)</h3></a>
-    <a href="year.php" class="w3-bar-item w3-button w3-text-grey w3-hover-black"><i class="fa fa-calendar-check-o" style="font-size:18px;color:red">&nbsp;&nbsp;</i><h3>Expenditure of year </h3></a>
+    <a href="year.php" class="w3-bar-item w3-button w3-text-grey w3-hover-black"><i class="fa fa-calendar-check-o navv" >&nbsp;&nbsp;</i><h3>Expenditure of year </h3></a>
 	<a href="month.php" class="w3-bar-item w3-button w3-text-grey w3-hover-black"><h3>Expenditure of year,month </h3></a>
 	<a href="day.php" class="w3-bar-item w3-button w3-text-grey w3-hover-black"><h3>Expenditure of year,month,day </h3></a>
 	<a href="update.php" class="w3-bar-item w3-button w3-text-grey w3-hover-black"><h3>Cost update (report of last bound to cost)</h3></a>
@@ -23,5 +24,5 @@
 
 
  <!-- font-awesome for sidebar -->
-<span class="w3-button w3-xxlarge  w3-right" style='color:white' onclick="w3_open()"><i class="fa fa-bars"></i></span>
+<span class="w3-button w3-xxlarge  w3-left" id='colorwhite' onclick="w3_open()"><i class="fa fa-bars"></i></span> 
 
