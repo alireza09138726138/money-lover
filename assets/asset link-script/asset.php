e97909070e7a3ed3f198ca3061f20c2a4bd087c7
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-07-01 01:19:03 +0430

    Update asset.php

--- assets/asset link-script/asset.php
+++ assets/asset link-script/asset.php
@@ -1,6 +1,8 @@
-<link rel="stylesheet" href="assets/style/style.css">
+  <link rel="stylesheet" href="assets/style/style.css">
+<link rel="stylesheet" href="assets/style/style index.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="shortcut icon" href="picture/pexels-photo-221174.png.jpg"/>
+<link rel="shortcut icon" href="assets/style/picture/pexels-photo-221174.png.jpg"/>
+
 <!-- font-awesome for sidebar -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -10,3 +12,4 @@
 <script language="javascript" type="text/javascript" src="ajax.js"></script>
 <script language="javascript" type="text/javascript" src="assets/js/app.js"></script>
 <script type="text/javascript" src="http://www.phpzag.com/demo/delete-records-with-bootstrap-confirm-modal-using-php-mysql/script/bootbox.min.js"></script>
+ 
