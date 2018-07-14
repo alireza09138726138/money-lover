ce6ad5692db2a0befc126035d33773d7a8bd15bd
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-07-14 22:22:58 +0430

    Update register.php

--- register.php
+++ register.php
@@ -1,30 +1,28 @@
-
- 
-  <?php 
- // فراخوانی منوی بالا
- // فراخوانی فایل دیتابیس
+<?php 
+  include("libs/smarty/Smarty.class.php");
 include 'configg.php';
- if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email'])){ 
+$result=false;
+$fla=0;
+$wrong_credentials=false;
+ if(isset($_POST['username']) && isset($_POST['password'])){ 
  
 
-     $username = mysqli_real_escape_string($conn, $_POST['username']); 
-     $email = mysqli_real_escape_string($conn, $_POST['email']); 
+     $username = mysqli_real_escape_string($conn, $_POST['username']);
+	  $email = mysqli_real_escape_string($conn, $_POST['email']);
      $password = md5($_POST['password']); 
      $sql = mysqli_query($conn, "SELECT * FROM user WHERE username = '".$username."'");
      if(mysqli_num_rows($sql)>0){ 
-         die("Username taken."); 
+         
+		$fla=1;
      }else{ 
-     mysqli_query($conn, "INSERT INTO user(username, password, email) VALUES('$username', '$password', '$email')") or die (mysqli_error($conn)); 
-	 echo "Account created."; 
+    $result = mysqli_query($conn, "INSERT INTO user(username, password, email) VALUES('$username', '$password', '$email')") or die (mysqli_error($conn)); 
+	
+		 }
  }
- } 
+
+$smarty = new smarty();
+		$smarty->assign('fla', $fla);
+		$smarty->assign('wrong_credentials', $wrong_credentials);
+		$smarty->assign('result', $result);
+		$smarty->display("register.tpl"); 
  ?>
- 
-  <html>
-   <form action="register.php" method="post">
-      Username: <input name="username" type="text" />
-      Password: <input type="password" name="password" />
-      Email: <input name="email" type="text" />
-      <input type="submit" value="Submit" />
-   </form>
- </html>
