1a2d572e56bc156ec2a2be502ee6dc58bc8d056b
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-07-14 21:50:32 +0430

    Update Report.php

--- Report.php
+++ Report.php
@@ -1,7 +1,7 @@
 <?php
 include("libs/smarty/Smarty.class.php");
 include 'configg.php';
-
+$include =include('session.php');
 $date = date("d-m-Y ");
 
 $honest_credentials = false;
@@ -9,7 +9,7 @@ $sum = null;
 $rows =null;
 
 
-$result = mysqli_query($conn, "SELECT DISTINCT Comment FROM produ WHERE !Price=''") or die(mysqli_error ($result));
+$result = mysqli_query($conn, "SELECT DISTINCT Comment FROM produ WHERE !Price='' AND user_id = '".$login_id."'") or die(mysqli_error ($result));
 $rowss = mysqli_fetch_all ($result, MYSQLI_ASSOC);
 
 
@@ -18,10 +18,10 @@ $show_table = $rowss > 0;
 
 	if (isset($_POST['submite'])) {
     $product= mysqli_real_escape_string($conn,$_POST['product']);
-    $result = mysqli_query($conn, "SELECT * FROM produ WHERE Comment='".$product."'") or die(mysqli ($result));
+    $result = mysqli_query($conn, "SELECT * FROM produ WHERE Comment='".$product."' AND user_id = '".$login_id."'") or die(mysqli ($result));
 	$rows = mysqli_fetch_all ($result, MYSQLI_ASSOC);
 	
-	$resultt = mysqli_query($conn,"SELECT sum(Price) FROM produ WHERE Comment='".$product."'") or die(mysqli_error());
+	$resultt = mysqli_query($conn,"SELECT sum(Price) FROM produ WHERE Comment='".$product."' AND user_id = '".$login_id."'") or die(mysqli_error());
     $row = mysqli_fetch_array($resultt);
     $sum = $row['sum(Price)'];
     $honest_credentials = mysqli_num_rows($result) > 0;
@@ -32,6 +32,8 @@ $show_table = $rowss > 0;
 
 $smarty = new smarty();
 $smarty->assign('date', $date);
+$smarty->assign('login_id', $login_id);
+$smarty->assign('login_session', $login_session);
 $smarty->assign('show_table', $show_table);
 $smarty->assign('honest_credentials', $honest_credentials);
 $smarty->assign('rowss', $rowss);
@@ -39,3 +41,4 @@ $smarty->assign('rows', $rows);
 $smarty->assign('sum', $sum);
 $smarty->display("Report.tpl");
 ?>
+
