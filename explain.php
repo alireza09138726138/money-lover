550249a0c869b600e69d111ee584783507866f25
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-07-14 22:05:36 +0430

    Update explain.php

--- explain.php
+++ explain.php
@@ -1,7 +1,7 @@
 <?php
 include("libs/smarty/Smarty.class.php");
 include 'configg.php';
-
+$include =include('session.php');
 $date = date("d-m-Y ");
 
 
@@ -11,7 +11,10 @@ $date = date("d-m-Y ");
 
 $smarty = new smarty();
 $smarty->assign('date', $date);
-
+$smarty->assign('login_id', $login_id);
+$smarty->assign('login_session', $login_session);
 $smarty->display("explain.tpl");
 ?>
+
+
 
