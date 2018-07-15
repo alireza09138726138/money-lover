82c6d735129613dca351cf9e59aa23689c40718c
alireza09138726138 <39461334+alireza09138726138@users.noreply.github.com>
2018-07-16 00:08:51 +0430

    Update session.php

--- session.php
+++ session.php
@@ -2,17 +2,14 @@
 include 'configg.php';
 session_start();// Starting Session
 // Storing Session
-$user_check=$_SESSION['login_user'];
+$user_check=$_SESSION['logged_in_user'];
 // SQL Query To Fetch Complete Information Of User
 $sql = mysqli_query($conn, "SELECT * FROM user WHERE username = '".$user_check."'");
 
 $row = mysqli_fetch_assoc($sql);
 $login_session =$row['username'];
 $login_id =$row['id'];
-if(!isset($login_session)){
-mysqli_close($conn); // Closing Connection
-header('Location: index.php'); // Redirecting To Home Page
-}
+
 ?>
 
 
